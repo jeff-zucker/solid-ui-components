06d035502ab11849d295d0887f7a988a81a5c58d
jeff-zucker <dubzed@gmail.com>
2021-04-01 12:45:55 -0700

    initial upload

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 - generate high level HTML widgets from RDF data
 
+**WORK-IN-PROGRES** - not ready for use yet
+
 ## Overview
 
 This library aims to support creating tabs, menus, tables, lists and other widgets directly from RDF.  It can be used dynamically in browser-based apps or can be run on the command line to create static HTML pages.  It is basically a front-end for the solid-ui library which allows you to use the powerful widgets of the SolidOS databrowser in your own apps without the complexity of the full SolidOS stack.
@@ -27,5 +29,18 @@ Please check out the [online demo](TBD).
   <script src="../src/solid-ui-components.js"></script>
 ```
 3. All done.  Now when you open tabs.html it will show an interactive tabset.
+
+@prefix : <#> .
+:Tabset :hasTabs ( :T1 :T2 ) .
+:T1 :label "x"; :uri <x> .
+:T2 :label "y"; :uri <y> .
+
+@prefix : <#> .
+@prefix n: <http://www.w3.org/2006/vcard/ns#>.
+
+:MyGroup a vcard:Group; vcard:hasMember :M1, :M2 .
+:M1 vcard:fn "x"; :tabUri <x> .
+:M2 :tabLabel "y"; :tabUri <y> .
+
 
 
---
+++ src/solid-uix.js
@@ -0,0 +1,94 @@
+const rdfs = UI.rdf.Namespace('http://www.w3.org/2000/01/rdf-schema#');
+const schema = UI.rdf.Namespace('http://schema.org/');
+
+export class SolidUIX {
+    
+  async init(){
+    for(let elm of document.getElementsByClassName('solid-ui-tabs')){
+      let t = await makeTabs( elm.dataset );
+      elm.appendChild( await t.render() );
+    }
+  }
+  
+  async makeTabs(dataset) {
+    let source = dataset.source;
+    const backgroundColor = dataset.backgroundcolor || undefined;
+    let orientation = dataset.orientation || 0;
+    if(orientation==="top") orientation = 0;
+    if(orientation==="left") orientation = 1;
+    if(orientation==="right") orientation = 2;
+    if(orientation==="bottom") orientation = 3;
+    let selected = dataset.selectedtab || 1;
+    selected = selected -1;
+    if(selected<0) selected = 0;
+    if(!source.startsWith('http')) {
+      source = location.href.replace(/\/[^\/]*$/,'/') + source;
+    }
+    const kb = await load(source);
+    const current = UI.rdf.Namespace(source+"#");
+    const subject = current('TabSet');
+    const predicate = current('hasTabs');
+    let items = kb.match( subject, predicate );
+    items = (items.length > 0) ? items[0].object.elements : [];
+    if(selected>items.length) selected = 0;
+    let tabset = {
+      render: async () => UI.tabs.tabWidget({
+        orientation:orientation,
+        backgroundColor:backgroundColor,
+        subject,
+        predicate,
+        items,
+        renderTab: async (tabDiv,tabSubject)=>{
+          tabDiv.dataset.name = tabSubject.uri;
+          let label = kb.any( tabSubject, rdfs('label') );
+          if(label) {
+            tabDiv.innerText = label.value;
+          }
+          else {
+            tabDiv.innerText = tabSubject.uri.replace(/.*#/,'');
+          }
+        },
+        renderMain: async (bodyMain, subject) => {
+          await renderLink( kb, subject, current, bodyMain )
+        },
+        selectedTab : selected ? items[selected].uri : 0
+      })
+    };
+    return(tabset);
+  }
+
+  async load( uri ){
+    let store = UI.rdf.graph();
+    let fetcher = UI.rdf.fetcher( store );
+    await fetcher.load( uri );
+    return store;
+  }
+
+  async renderLink ( kb, subject, current, containingElement ){
+    let url = kb.any( subject, schema('url') );
+    let handler = kb.any( subject, schema('potentialAction') );
+    if( handler ) {
+      let content = "";
+      let r = await fetch(url.uri);
+      content = await r.text();
+      if(handler=="preformat") {
+        let pre = document.createElement('PRE');
+        pre.appendChild( document.createTextNode(content) );
+        containingElement.innerHTML = "";
+        containingElement.appendChild(pre);
+        return;
+      }
+    }
+    let iframe = document.createElement('IFRAME');
+    iframe.src = url.uri;
+    iframe.width="100%";
+    iframe.height="100%";
+    iframe.style.border="none";
+    
+    containingElement.innerHTML = "";
+    containingElement.appendChild(iframe);
+  }
+
+}
+
+// THE END!
